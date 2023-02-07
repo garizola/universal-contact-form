0a0d112993621e124b2574994b749b06410c63c9
garizola <99741841+garizola@users.noreply.github.com>
2023-02-07 14:44:29 -0500

    theme

--- config/theme.js
+++ config/theme.js
@@ -1,10 +1,21 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#d3d3d3 ",
+      // ...
+      900: "#1a202c",
+    },
+  },
+})
+
+
 const config = {
   initialColorMode: "dark",
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+// const theme = extendTheme({ config });
 
 export default theme;
